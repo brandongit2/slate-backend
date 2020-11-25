23a7c42e16dd700edd94aad4ba1e658de460b771
Brandon Tsang <brandononline2@gmail.com>
2020-11-25 01:43:42 -0500

    Added `hyphenateField` query.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import cors from 'cors';
-import express from 'express';
+import express, { Request, Response } from 'express';
+// @ts-ignore
+import Hyphenopoly from 'hyphenopoly';
 import { ObjectID } from 'mongodb';
 
 import * as db from './db';
@@ -10,25 +12,58 @@ const port = 3001;
 
 app.use(cors());
 
-// /list-subjects
-app.get(apiUrl + 'list-subjects', async (req, res) => {
-    res.json(await db.listSubjects());
+const hyphenate = Hyphenopoly.config({
+    require: ['en-us'],
+    sync: true
 });
+
+function handleRoute(fn: (req: Request, res: Response) => Promise<any[]>) {
+    return async (req: Request, res: Response) => {
+        let data = await fn(req, res);
+
+        // Provide a list of fields for which their values will by hyphenated.
+        // ?hyphenateFields=<field>[,<field>...]
+        if (req.query.hyphenateFields) {
+            let hyphenateFields = (<string>req.query.hyphenateFields).split(
+                ','
+            );
+
+            data = data.map((obj) => {
+                hyphenateFields.forEach((field) => {
+                    if (obj[field]) obj[field] = hyphenate(obj[field]);
+                });
+
+                return obj;
+            });
+        }
+
+        res.json(data);
+    };
+}
+
+// /list-subjects
+app.get(
+    `${apiUrl}list-subjects`,
+    handleRoute((req, res) => db.listSubjects())
+);
 
 // /get-subject?name=<name>
-app.get(apiUrl + 'get-subject', async (req, res) => {
-    res.json(await db.getSubject(<string>req.query.name));
-});
+app.get(
+    apiUrl + 'get-subject',
+    handleRoute((req, res) => db.getSubject(<string>req.query.name))
+);
 
 // /get-data?id=<id>
-app.get(apiUrl + 'get-data', async (req, res) => {
-    res.json(await db.getData(new ObjectID(<string>req.query.id)));
-});
+app.get(
+    apiUrl + 'get-data',
+    handleRoute((req, res) => db.getData(new ObjectID(<string>req.query.id)))
+);
 
 // /get-children?id=<id>
-app.get(apiUrl + 'get-children', async (req, res) => {
-    res.json(await db.getChildren(<string>req.query.id));
-});
+app.get(
+    apiUrl + 'get-children',
+    handleRoute((req, res) => db.getChildren(<string>req.query.id))
+);
 
 app.get(apiUrl, (req, res) => {
     res.send('Slate API');
